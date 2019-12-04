3a7bd050000365a8afc2ca69eca7bbe337021808
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-04 08:37:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Add your functions here
+let map = function(sourceArray,sourceFunction){
+    return sourceArray.map(sourceFunction);
+  }
+let reduce = function(sourceArray,callBack,startingPoint){
+    
+    return sourceArray.reduce(callBack);
+  }
